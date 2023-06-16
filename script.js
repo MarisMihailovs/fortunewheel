f83988acc4e0cd78e03fb1462695c3ffedafb1ce
MarisMihailovs <marismihailovs@gmail.com>
2023-06-16 13:27:08 +0300

    audio added

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const wheel = document.querySelector('.wheel');
 const spinBtn = document.querySelector('.spinBtn');
-
+const audio = document.querySelector('audio');
 let value = Math.ceil(Math.random() * 3600);
 
 spinBtn.addEventListener('click', () => {
     wheel.style.transform = `rotate(${value}deg)`;
     value += Math.ceil(Math.random() * 3600);
-})
+    audio.play();
+    setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0;
+    }, 5000);
+});
